5905c93639eb16c5004a488b1736566a4f32b473
MarkyMalarkey <markito776@yahoo.com>
2020-04-25 16:08:24 -0400

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -20,7 +20,7 @@
                 } else {
                     session_start();
                     $_SESSION['is_valid_admin'] = TRUE;
-                    include("admin_index.php");
+                    header("admin_index.php");
                 }
             }
         ?>
