2db2e7ea2a8c613519dd78a79384c09e4f110db7
MarkyMalarkey <markito776@yahoo.com>
2020-04-25 15:53:49 -0400

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=api_db';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=e9q3zaqmobg4r9kg';
+    $username = 'asxybhpo6jm6w8ge';
+    $password = 'fnaz3cv3bmwdl8t5';
 
     try {
         $db = new PDO($dsn, $username, $password);
